cb79cd75fa966a81cb1254d1bbb1830ed77bae29
victorlesg <victorlesg.2004@gmail.com>
2023-10-26 14:10:21 -0300

    projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+var elementoDuvida = document.querySelectorAll('.duvida')
+
+elementoDuvida.forEach(function (duvida){
+    duvida.addEventListener('click', function () {
+        duvida.classList.toggle("ativa")
+    })
+})
+
+//function nome(argumento) {
+//}
